d07eadec552103a42999defa2fd177b3ebe1404c
sagedash <maggie@usgeocoder.com>
2025-11-12 16:43:10 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    assetsInclude: ['**/_redirects'],
     rollupOptions: {
       output: {
         manualChunks: undefined
